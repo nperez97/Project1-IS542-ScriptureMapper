d8cd19875a7f10a1911efde42730bec52d67c011
nperez97 <66038086+nperez97@users.noreply.github.com>
2022-02-04 16:03:30 -0700

    Navigate Home - Part 7

--- scriptures.js
+++ scriptures.js
@@ -4,12 +4,13 @@ const Scriptures = (function () {
     //---------------------------------CONSTANTS---------------------------------
     const REQUEST_GET = "GET";
     const CLASS_BOOKS = "books";
+    const CLASS_BUTTON = "btn";
     const CLASS_VOLUME = "volume";
     const DIV_SCRIPTURES_NAVIGATOR = "scripnav";
     const DIV_SCRIPTURES = "scriptures";
     const REQUEST_STATUS_OK = 200;
     const REQUEST_STATUS_ERROR = 400;
-    const TAG_HEADERS = "h5";
+    const TAG_HEADER5 = "h5";
     const URL_BASE = "https://scriptures.byu.edu/";
     const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`;
     const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`;
@@ -25,6 +26,8 @@ const Scriptures = (function () {
     //-------------------------PRIVATE METHOD DECLARATIONS---------------------------------
     let ajax;
     let bookChapterValid;
+    let booksGrid;
+    let booksGridContent;
     let cacheBooks;
     let htmlAnchor;
     let htmlDiv;
@@ -37,6 +40,7 @@ const Scriptures = (function () {
     let init;
     let onHashChanged;
     let testGeoplaces;
+    let volumesGridContent;
 
     //---------------------------------PRIVATE METHODS---------------------------------
     ajax = function (url, successCallback, failureCallback) {
@@ -75,6 +79,28 @@ const Scriptures = (function () {
         }
 
         return true;
+    };
+
+    booksGrid = function (volume) {
+        return htmlDiv({
+            classKey: CLASS_BOOKS,
+            content: booksGridContent(volume)
+        });
+    };
+
+    booksGridContent = function (volume) {
+        let gridContent = "";
+
+        volume.books.forEach(function (book) {
+            gridContent += htmlLink({
+                classKey: CLASS_BUTTON,
+                id: book.id,
+                href: `#${volume.id}:${book.id}`,
+                content: book.gridName
+            });
+        });
+
+        return gridContent;
     };
 
     cacheBooks = function (callback){
@@ -190,12 +216,10 @@ const Scriptures = (function () {
     };
 
     navigateHome = function (volumeId) {
-        document.getElementById(DIV_SCRIPTURES).innerHTML = 
-        "<div>Old Testament</div>" +
-        "<div>New Testament</div>" +
-        "<div>Book of Mormon</div>" +
-        "<div>Doctrine and Covenants</div>" +
-        "<div>Pearl of Great Price</div>" + volumeId;
+        document.getElementById(DIV_SCRIPTURES).innerHTML = htmlDiv({
+            id: DIV_SCRIPTURES_NAVIGATOR,
+            content: volumesGridContent(volumeId)
+        });
     };
 
     onHashChanged = function () {
@@ -288,6 +312,23 @@ const Scriptures = (function () {
 
         makeUniqueGeoPlaces(geoplaces);
     }
+
+    volumesGridContent = function (volumeId) {
+        let gridContent = "";
+
+        volumes.forEach(function (volume) {
+            if (volumeId === undefined || volumeId === volume.id) {
+                gridContent += htmlDiv({
+                    classKey: CLASS_VOLUME,
+                    content: htmlAnchor(volume) + htmlElement(TAG_HEADER5, volume.fullName)
+                });
+
+                gridContent += booksGrid(volume);
+            }
+        });
+        return gridContent;
+    };
+
     //---------------------------------PUBLIC API---------------------------------
 
 
