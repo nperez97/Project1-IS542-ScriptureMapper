14ea4d4d168f69e05938cdbaf4c58eabdcd32ee2
nperez97 <66038086+nperez97@users.noreply.github.com>
2022-02-04 13:54:21 -0700

    Video 6 - on hash changed

--- scriptures.js
+++ scriptures.js
@@ -5,8 +5,15 @@ const Scriptures = (function () {
     const REQUEST_GET = "GET";
     const CLASS_BOOKS = "books";
     const CLASS_VOLUME = "volume";
+    const DIV_SCRIPTURES_NAVIGATOR = "scripnav";
+    const DIV_SCRIPTURES = "scriptures";
     const REQUEST_STATUS_OK = 200;
     const REQUEST_STATUS_ERROR = 400;
+    const TAG_HEADERS = "h5";
+    const URL_BASE = "https://scriptures.byu.edu/";
+    const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`;
+    const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`;
+
 
 
 
@@ -18,9 +25,15 @@ const Scriptures = (function () {
     //-------------------------PRIVATE METHOD DECLARATIONS---------------------------------
     let ajax;
     let cacheBooks;
+    let htmlAnchor;
+    let htmlDiv;
+    let htmlElement;
+    let htmlLink;
+    let htmlHashLink;
+    let navigateHome;
     let init;
-    let testGeoplaces;
     let onHashChanged;
+    let testGeoplaces;
 
     //---------------------------------PRIVATE METHODS---------------------------------
     ajax = function (url, successCallback, failureCallback) {
@@ -65,6 +78,66 @@ const Scriptures = (function () {
         }
     };
 
+    htmlAnchor = function (volume) {
+        return `<a name="v${volume.id}"><a/>`;
+    };
+    
+    
+    htmlDiv = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let idString = "";
+    
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+        
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+        
+        if (parameters.id !== undefined) {
+            idString = ` id="${parameters.id}"`;
+        }
+        
+        return `<div${idString}${classString}>${contentString}</div>`;
+    };
+    
+    htmlElement = function (tagName, content) {
+        return `<${tagName}>${content}</${tagName}>`;
+    }
+    
+    
+    
+    htmlLink = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let hrefString = "";
+        let idString = "";
+    
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+        
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+        
+        if (parameters.href !== undefined) {
+            hrefString = ` href="${parameters.href}"`;
+        }
+    
+        if (parameters.id !== undefined) {
+            idString = ` id="${parameters.id}"`;
+        }
+    
+        return `<a${idString}${classString}${hrefString}>${contentString}</a>`;
+    };
+    
+    htmlHashLink = function (hashArguments, content) {
+        return `<a href="javascript:void(0)" onclick="changeHash(${hashArguments})">${content}</a>`;
+    };
+
     init = function (callback) {
         let booksLoaded = false;
         let volumesLoaded = false;
@@ -91,9 +164,34 @@ const Scriptures = (function () {
         );
     };
 
-    onHashChanged = function () {
-        console.log(window.location.hash)
+    navigateHome = function (volumeId) {
+        document.getElementById(DIV_SCRIPTURES).innerHTML = 
+        "<div>Old Testament</div>" +
+        "<div>New Testament</div>" +
+        "<div>Book of Mormon</div>" +
+        "<div>Doctrine and Covenants</div>" +
+        "<div>Pearl of Great Price</div>" + volumeId;
     }
+
+    onHashChanged = function () {
+        let ids = [];
+
+        if (location.hash !== "" && location.hash.length > 1){
+            ids = location.hash.slice(1).split(":");
+        }
+
+        if (ids.length <= 0 ) {
+            navigateHome();
+        } else if (ids.length === 1){
+            let volumeId = Number(ids[0]);
+
+            if (volumeId < volumes[0].id || volumeId > volumes.slice(-1)[0].id){
+                navigateHome();
+            } else {
+                navigateHome(volumeId);
+            }
+        }
+    };
 
 
     testGeoplaces = function () {
